4b90d605bc1a4aae1067f5d7d92315a20791eea5
tanaxer01 <jpbrione@gmail.com>
2023-10-11 08:39:02 -0300

    :sparkle: DQNAgent

--- Dockerfile
+++ Dockerfile
@@ -19,9 +19,9 @@ WORKDIR /app
 RUN apt update -y && apt install -y git && python -m venv /opt/venv
 ENV PATH="/opt/venv/bin:$PATH"
 
-RUN git clone https://github.com/tanaxer01/batsim-py && cd batsim-py && pip install .
-RUN git clone https://github.com/tanaxer01/GridGym   && cd GridGym   && pip install .
-RUN pip install evalys jupyter pandas matplotlib
+RUN pip install git+https://github.com/tanaxer01/batsim-py \
+                git+https://github.com/tanaxer01/GridGym   \
+                torch
 
 FROM python:3.10-slim
 
@@ -30,7 +30,6 @@ COPY --from=builder /output/profile /usr/.local
 
 COPY --from=builder2 /opt/venv /opt/venv
 
-#ENV PATH=/usr/.local/bin:$PATH
 ENV PATH="/opt/venv/bin:/usr/.local/bin:$PATH"
 
 CMD ["bash"]
--- gym_test.py
+++ gym_test.py
@@ -1,10 +1,14 @@
-import argparse
-
-import gym
 import batsim_py
+import gym
 
-from schedulers.FCFSScheduler import FCFSScheduler
-import gridgym.envs.off_reservation_env as e
+import numpy as np
+
+
+from schedulers.DQNAgent import DQNAgent
+#import gridgym.envs.off_reservation_env as e
+#import gridgym.envs.scheduling_env as e
+#import test_env as e
+import scheduling_env as e
 
 
 class FirstFitScheduler():
@@ -12,8 +16,13 @@ class FirstFitScheduler():
         agenda = obs['platform']['agenda']
         queue = obs['queue']['jobs']
         nb_available = len(agenda) - sum(1 for j in agenda if j[1] != 0)
-        job_pos = next((i for i, j in enumerate(queue)
-                        if 0 < j[1] <= nb_available), -1)
+        job_pos = next((i for i, j in enumerate(queue) if 0 < j[1] <= nb_available), -1)
+
+        print(obs["platform"]["status"])
+        curr = obs["current_time"]
+        if not np.all(queue == 0):
+            res = [ curr - i if i != 0 else 0 for i in queue[: , 0]  ]
+
         return job_pos + 1
 
     def play(self, env, verbose=True):
@@ -25,6 +34,9 @@ class FirstFitScheduler():
             history['steps'] += 1
             history['info'] = info
 
+            if history["score"] < -3000:
+                done = True
+
         if verbose:
             print(
                 "[DONE] Score: {} - Steps: {}".format(history['score'], history['steps']))
@@ -35,33 +47,40 @@ class FirstFitScheduler():
 def run_gym():
     print("[RUNNING]")
 
-    agent = FirstFitScheduler()
+    agent  = FirstFitScheduler()
 
     env = gym.make("gridgym:Scheduling-v0",
-                   platform_fn="/data/platforms/energy_platform_mod.xml",
+                   #platform_fn="/data/platforms/FatTree/generated.xml",
+                   platform_fn="/data/platforms/platform.xml",
                    workloads_dir="/data/workloads/test",
                    t_action=1,
                    queue_max_len=20,
                    t_shutdown=5,
-                   hosts_per_server=12)
+                   hosts_per_server=2)
+
+    env2 = e.SchedulingEnv(
+                   platform_fn="/data/platforms/FatTree/generated.xml",
+                   #platform_fn="/data/platforms/platform.xml",
+                   workloads_dir="/data/workloads/test",
+                   t_action=1,
+                   queue_max_len=20,
+                   t_shutdown=5,
+                   hosts_per_server=2)
 
     jobs_mon = batsim_py.monitors.JobMonitor(env.simulator)
     sim_mon  = batsim_py.monitors.SimulationMonitor(env.simulator)
+    schedule_mon  = batsim_py.monitors.SchedulerMonitor(env.simulator)
 
+    agent = DQNAgent()
     hist = agent.play(env, True)
 
     print("[DONE]")
-    return jobs_mon, sim_mon, hist
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env_id", default="gridgym:Scheduling-v0", type=str)
-    # Agent specific args
-    parser.add_argument("--queue_sz", default=20, type=int)
-    parser.add_argument("--t_action", default=1, type=int)
-    return parser.parse_args()
+    return jobs_mon, sim_mon, schedule_mon, hist
 
 
 if __name__ == "__main__":
-    run_gym()
+    jobs_df, sim_df, schedule_df, _ = run_gym()
+
+    jobs_df.to_csv("/data/expe-out/jobs-DQN.out")
+    sim_df.to_csv("/data/expe-out/sim-DQN.out")
+    schedule_df.to_csv("/data/expe-out/schedule-DQN.out")
